014f9bc7256c15be71024b0c9cc653db078de7c2
Kawere Wagaba <ckwagaba@gmail.com>
2021-08-20 11:47:14 +0300

    fix/hack cert issue TODO: work it out with backend

--- index.js
+++ index.js
@@ -30,25 +30,28 @@ const pushImage = async (res, docker, imageName) => {
     await docker.command(`push ${imageName}`);
 
     // deploy
-    const project = "1f320c75-8b42-401a-9d10-d0b07eabf2c6"; // TODO: shall be dynamic
+    const project = "37cc628c-5b9e-4b21-b41d-4d23e4c71769"; // TODO: shall be dynamic
     const token =
       "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjkyNzQ0OTAsIm5iZiI6MTYyOTI3NDQ5MCwianRpIjoiNjcyNWEyZTktNWI3Yy00MjMzLTliMWItYWZlNTEwMGEyNzUzIiwiZXhwIjoxNjMwMTM4NDkwLCJpZGVudGl0eSI6ImJlZDVjODQwLTQ3MjUtNDczYi05MjA1LThiZWM2YThlNWM2YSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbeyJpZCI6IjdmN2ZiZGQ5LWMxMGQtNGRiMC1iOTQ3LWUyZDc0MmE2MTlhOSIsIm5hbWUiOiJjdXN0b21lciJ9XX19.0GQ8LnIjm5JLqMyzqfe-9kV2XIeYU_jNVl51TCJt5sc";
 
-    axios
-      .post(
-        `${BASE_URL}/projects/${project}/apps`,
-        {
-          image: imageName,
-          name: "hello",
+    const result = await axios.post(
+      `${BASE_URL}/projects/${project}/apps`,
+      {
+        env_vars: {},
+        image: imageName,
+        name: "hello",
+        project_id: project,
+        private_image: false,
+        replicas: 1,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      )
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err.response));
+      }
+    );
+
+    console.log(result.data);
   } catch (err) {
     console.log(err);
 
