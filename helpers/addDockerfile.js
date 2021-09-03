95bd52fd83057099d544e765e9500bdf808f8996
StephenAraka <stevenaraka@gmail.com>
2021-09-03 10:12:20 +0200

    Update code

--- helpers/addDockerfile.js
+++ helpers/addDockerfile.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 /**
  * Add a preset Dockerfile
  */
- const addDockerfile = (dir) => {
+ const addDockerfile = (dir, framework) => {
   fs.copyFile('./preset_dockerfiles/html-nginx.txt', `./uploads/${dir}/Dockerfile`, (err) => {
     if (err) throw err;
     console.log('Dockerfile copied to destination.txt');
