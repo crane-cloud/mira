af8bda3d27e29a9cb8476bbce7dc35d317335dce
inno-asiimwe <asiimwe@outlook.com>
2021-10-02 20:56:54 +0300

    Allow traffic from 0.0.0.0

--- index.js
+++ index.js
@@ -68,6 +68,6 @@ app.post("/", createAppDir, upload.array("files"), async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`...listening on ${PORT}`);
 });
