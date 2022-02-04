14466330d43c422a2e459ef56ac4b93b1c6dd2b0
Henry Mutegeki <hmutegeki@Henrys-MacBook-Pro.local>
2022-02-04 09:19:25 +0300

    add django app support files

--- helpers/addDockerfile.js
+++ helpers/addDockerfile.js
@@ -14,7 +14,28 @@ const frameworksmapping = require("../helpers/frameworksmapping");
       console.log('Docker ignore copied to destination.txt');
       callback(null);
     });
+
+    // check for django and add 2 files to app directory
+    if(framework == "Django"){
+
+      fs.copyFile( './django_xtras/start-server.txt', `${dir}/start-server.sh`, (err) => {
+        if (err) callback(err);
+        console.log('Start script copied to destination');
+        callback(null);
+      });
+
+      fs.copyFile( './django_xtras/nginx_default.txt', `${dir}/nginx.default`, (err) => {
+        if (err) callback(err);
+        console.log('Nginx file copied to destination');
+        callback(null);
+      });
+
+    }
+ 
+
   });
+
+
 }
 
 module.exports = addDockerfile;
